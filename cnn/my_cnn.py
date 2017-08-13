841625d50bbc927e54f6c460d644b6a1bd02556c
Abner Chen <abner@17.media>
2017-08-13 16:56:11 +0800

    Add CNN exercise

--- cnn/my_cnn.py
+++ cnn/my_cnn.py
@@ -9,17 +9,27 @@ from keras.optimizers import SGD
 from keras.utils import np_utils
 
 # set dataset path
-dataset_path = '../cifar_10/'
+dataset_path = 'cifar_10/'
 exec(open("read_dataset2img.py").read())
 
 '''CNN model'''
 model = Sequential()
 # 請建立一個 CNN model
-
 # CNN
+model.add(Convolution2D(32, 3, 3, border_mode='same', input_shape=X_train[0].shape))
+model.add(Activation('relu'))
+model.add(Convolution2D(32, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
 
 model.add(Flatten())
 #DNN
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10))
+model.add(Activation('softmax'))
 
 
 '''setting optimizer'''
@@ -36,14 +46,20 @@ batch_size = 128
 epoch = 5
 # validation data comes from training data
 # model.fit(X_train, Y_train, batch_size=batch_size,
-# 	      nb_epoch=epoch, validation_split=0.1, shuffle=True)
+#           nb_epoch=epoch, validation_split=0.1, shuffle=True)
 
 # validation data comes from testing data
-fit_log = model.fit(X_train, Y_train, batch_size=batch_size,
-	                epochs=epoch, validation_data=(X_test, Y_test), shuffle=True)
+fit_log = model.fit(
+    X_train,
+    Y_train,
+    batch_size=batch_size,
+    epochs=epoch,
+    validation_data=(X_test, Y_test),
+    shuffle=True)
 
 '''saving training history'''
 import csv
+output_fn = 'test'
 exec(open("write_csv.py").read())
 
 '''saving model'''
